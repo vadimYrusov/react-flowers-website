a494ca3c026ce738fbaae367002f026dc5ec02f1
vadimYrusov <yurusovvadim845@gmail.com>
2022-10-09 23:55:28 +0300

    Navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -17,13 +18,17 @@ const showButton = () => {
   }
 };
 
+useEffect(() => {
+  showButton();
+}, []);
+
 window.addEventListener('resize', showButton);
 
   return (
        <>
        <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                     FLWRS 
                 </Link>
                 <div className="menu-icon" onClick={handClick}>
